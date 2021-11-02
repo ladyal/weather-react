7b07e250f6b565b074a02f7ae94b149cb7cc0973
ladyal <allison.m.brackeen@gmail.com>
2021-11-01 18:38:01 -0700

    NPX create-react-app .

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import SearchEngine from "./SearchEngine";
-import "./App.css";
-function App() {
-  return (
-    <div className="App">
-      <h1>Weather App</h1>
-      <SearchEngine />
-    </div>
-  );
-}
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
